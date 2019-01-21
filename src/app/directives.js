41ece69a1e3132857f0c18caf870a5fc6c41c079
Carlos Gonzalez <carlos.gonzalez@sitiouno.com>
2019-01-20 21:29:02 -0400

    Avances

--- src/app/directives.js
+++ src/app/directives.js
@@ -71,11 +71,11 @@
       link: function(scope, element) {
         var listener = function(event, toState)  {
           // Default title - load on Dashboard 1
-          var title = 'OneTip | Dashboard';
+          var title = 'EulerAcademy';
 
           //TEAM => se modifica el titulo de la pestana de HOME
           // Create your own title pattern
-          if (toState.data && toState.data.pageTitle) title = 'Flexipos | ' + toState.data.pageTitle;
+          if (toState.data && toState.data.pageTitle) title = 'EulerAcademy | ' + toState.data.pageTitle;
           $timeout(function() {
             element.text(title);
           });
